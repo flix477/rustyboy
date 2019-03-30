6fa631243afd64707c75ef0118b2fed6412670ae
Félix Léveillé <flxleveille@gmail.com>
2019-03-30 11:53:56 -0400

    wip

--- src/debugger/command.rs
+++ src/debugger/command.rs
@@ -1,4 +1,7 @@
 use crate::processor::registers::RegisterType;
+use crate::debugger::command::Command::Breakpoint;
+use crate::processor::Processor;
+use crate::bus::Bus;
 
 pub enum StatusType {
     Address(u16),
@@ -7,11 +10,11 @@ pub enum StatusType {
 }
 
 impl StatusType {
-    pub fn parse(values: Vec<&str>) -> Option<StatusType> {
+    pub fn parse(values: &[&str]) -> Option<StatusType> {
         let status = *values.get(0)?;
         match status {
             "register" | "r" => {
-                let register = values.get(1)?.to_lowercase().as_str();
+                let register = values.get(1)?;
                 let register = StatusType::parse_register(register)?;
                 Some(StatusType::Register(register))
             },
@@ -40,9 +43,32 @@ impl StatusType {
     }
 }
 
+pub enum BreakpointAction {
+    Add(u16),
+    Remove(u16),
+    List
+}
+
+impl BreakpointAction {
+    pub fn parse(values: &[&str]) -> Option<BreakpointAction> {
+        let action = *values.get(0)?;
+        match action {
+            "add" | "a" => {
+                None
+            },
+            "remove" | "r" => {
+                None
+            },
+            "list" | "l" => Some(BreakpointAction::List),
+            _ => None
+        }
+    }
+}
+
 pub enum Command {
     Continue,
     Help,
+    Breakpoint(BreakpointAction),
     Status(StatusType),
     StepInto,
     StepOver,
@@ -51,13 +77,17 @@ pub enum Command {
 
 impl Command {
     pub fn parse(value: &str) -> Option<Command> {
-        let separated = value.split(' ');
+        let separated: Vec<&str> = value.split(' ').collect();
 
         match separated[0] {
             "continue" | "c" => Some(Command::Continue),
             "help" | "h" | "?" => Some(Command::Help),
+            "breakpoint" | "b" => {
+                let action = BreakpointAction::parse(&separated[1..])?;
+                Some(Command::Breakpoint(action))
+            }
             "status" | "s" => {
-                let status = StatusType::parse(separated[1..])?;
+                let status = StatusType::parse(&separated[1..])?;
                 Some(Command::Status(status))
             }
             "step_into" | "si" => Some(Command::StepInto),
@@ -67,3 +97,17 @@ impl Command {
         }
     }
 }
+
+struct Debugger {
+    pub breakpoints: Vec<u16>
+}
+
+struct Command2 {
+    pub matching_values: Vec<&'static str>,
+    pub callback: Box<FnMut(&mut Debugger, &Processor, &Bus)>
+}
+
+const LOL: Command2 = Command2 {
+    matching_values: vec!["lol"],
+    callback: Box::new(|debugger, cpu, bus| {})
+};
--- src/debugger/mod.rs
+++ src/debugger/mod.rs
@@ -1,1 +1,21 @@
+use std::io::{self, Read};
+use self::command::Command;
+use crate::bus::Bus;
+use crate::processor::Processor;
+
 pub mod command;
+
+pub fn interpret<H: Bus>(cpu: &Processor, bus: &H) {
+    loop {
+        let mut input = String::new();
+        io::stdin().read_to_string(&mut input);
+        if let Some(cmd) = Command::parse(input.as_str()) {
+            execute(cpu, bus, cmd);
+        } else {
+            println!("ayo read the fucking help");
+        }
+    }
+}
+
+pub fn execute<H: Bus>(cpu: &Processor, bus: &H, cmd: Command) {
+}
