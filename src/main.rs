20966f84f3c21222d0bd5fb769fd8f27b52f698c
Félix Léveillé <flxleveille@gmail.com>
2019-05-22 23:03:20 -0400

    removed comments

--- src/main.rs
+++ src/main.rs
@@ -21,30 +21,12 @@ use crate::processor::instruction::Mnemonic;
 fn main() {
 //    let cartridge = Cartridge::from_file("test/individual/11-op a,(hl).gb").unwrap();
     let cartridge = Cartridge::from_file("tetris.gb").unwrap();
-    // 01 -> passed
-    // 02 -> passed
-    // 03 -> failed (ALL TESTS -> something's wrong in the setup)
-    // 04 -> passed
-    // 05 -> passed
-    // 06 -> passed
-    // 07 -> passed
-    // 08 -> passed
-    // 09 -> passed
-    // 10 -> passed
-    // 11 -> stalled
     let config = Config {
         cartridge,
         device_type: DeviceType::GameBoy,
         debugger_config: Some(DebuggerState {
             forced_break: false,
-            breakpoints: vec![
-//                Breakpoint {
-//                    line: 0xdef9,
-//                    conditions: Some(vec![
-//                        BreakpointCondition::MnemonicEquals(Mnemonic::SLA),
-//                    ])
-//                }
-            ]
+            breakpoints: vec![]
         }),
     };
 
