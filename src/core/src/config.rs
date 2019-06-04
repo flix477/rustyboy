37723eb8f9020776f4feee71cc0f58cf64babfbc
Félix Léveillé <flxleveille@gmail.com>
2019-06-04 18:52:12 -0400

    src -> packages

--- src/core/src/config.rs
+++
@@ -1,9 +0,0 @@
-use crate::cartridge::Cartridge;
-use crate::debugger::DebuggerState;
-use crate::gameboy::DeviceType;
-
-pub struct Config {
-    pub cartridge: Cartridge,
-    pub device_type: DeviceType,
-    pub debugger_config: Option<DebuggerState>,
-}
--- src/core/src/util/parse_hex.rs
+++
@@ -1,6 +0,0 @@
-pub fn parse_hex(value: &str) -> Option<u16> {
-    let lowercased = value.to_lowercase();
-    let trimmed = lowercased.trim_start_matches("0x");
-
-    u16::from_str_radix(trimmed, 16).ok()
-}
