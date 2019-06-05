d041a48d65645c8c8e913321137aa8f39046fc65
Félix Léveillé <flxleveille@gmail.com>
2019-06-05 18:05:10 -0400

    added better(?) ci config

---
+++ run-on-modified.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+PACKAGES_ROOT=packages/
+COMMIT="refs/remotes/origin/master"
+COMMAND=$1
+
+CHANGED_PACKAGES=$(git diff --dirstat=files,0 $COMMIT | sed -r "s/[0-9.[:space:]]+% //g" | sed -r "/(.+\/){3,}/d" | grep $PACKAGES_ROOT)
+
+if [[ $CHANGED_PACKAGES =~ "core" ]]; then
+    CHANGED_PACKAGES=$(ls -1a -d $PACKAGES_ROOT/*/)
+fi
+
+if [[ -z $CHANGED_PACKAGES ]]; then
+    echo Nothing to do.
+    exit 0
+fi
+
+CWD=$(pwd)
+
+while read -r line; do
+    cd $line
+    eval $COMMAND
+    cd $CWD
+done <<< "$CHANGED_PACKAGES"
