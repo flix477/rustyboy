0cc2449a8636d05d1a5704a0d2fb7b42ff3a43cf
Felix Leveille <flxleveille@gmail.com>
2019-06-07 14:18:40 -0400

    cartridge info display

--- packages/pc/src/app.rs
+++ packages/pc/src/app.rs
@@ -9,6 +9,8 @@ use rustyboy_core::gameboy::{DeviceType, Gameboy};
 use crate::keymap::keymap;
 use crate::shell_debugger::{DebuggerState, ShellDebugger};
 use crate::window::{background::BackgroundWindow, screen::MainWindow, Window};
+use rustyboy_core::cartridge::cartridge_metadata::CartridgeMetadata;
+use std::process::exit;
 
 pub fn run() {
     let matches = App::new("rustyboy")
@@ -16,12 +18,18 @@ pub fn run() {
         .about("Gameboy emulator written in Rust.")
         .args_from_usage(
             "<rom_path> 'ROM path'
-            -d, --debug 'Enable debugger'",
+            -d, --debug 'Enable debugger'
+            -i, --info 'Print cartridge metadata'",
         )
         .get_matches();
 
     let path = matches.value_of("rom_path").unwrap();
     let cartridge = Cartridge::from_file(path).unwrap();
+
+    if matches.is_present("info") {
+        print_cartridge_info(cartridge.metadata());
+        exit(0);
+    }
 
     let debugger = if matches.is_present("debug") {
         Some(Box::new(ShellDebugger::from_state(DebuggerState {
@@ -37,6 +45,23 @@ pub fn run() {
         debugger,
     };
     start_emulation(cartridge, config);
+}
+
+fn print_cartridge_info(metadata: &CartridgeMetadata) {
+    println!("Cartridge metadata");
+    println!("{}", "-".repeat(20));
+
+    println!("Title: {}", metadata.title);
+    println!("Region: {:?}", metadata.destination);
+    println!("Version: {}", metadata.version);
+    println!("GameBoy Color support: {:?}", metadata.cgb_flag);
+    println!("Super GameBoy enhancements: {:?}", metadata.sgb_enhanced);
+    println!("Manufacturer code: {:?}", metadata.manufacturer_code);
+    println!("New licencee code: {:?}", metadata.new_licensee_code);
+    println!("Old licencee code: {:?}", metadata.old_licensee_code);
+    println!("Cartridge capabilities: {:?}", metadata.capabilities);
+    println!("ROM size: {:?}", metadata.rom_size);
+    println!("RAM size: {:?}", metadata.ram_size);
 }
 
 fn start_emulation(cartridge: Cartridge, config: Config) {
