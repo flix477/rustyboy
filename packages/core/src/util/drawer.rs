b07c3ff1a7c08c9f8d01b28790befc96c2844cea
Felix Leveille <flxleveille@gmail.com>
2019-06-17 18:06:10 -0400

    Fix screen edge sprite bug (#13)

--- packages/core/src/util/drawer.rs
+++ packages/core/src/util/drawer.rs
@@ -21,35 +21,73 @@ pub fn draw_entity(
     buf: &mut Vec<DrawnColor>,
     palette: &Palette,
 ) {
-    draw_entity_with_options(entity, dimensions, buf, palette, false, false);
+    draw_entity_with_options(entity, dimensions, buf, palette, false, false, (0, 0));
+}
+
+pub fn draw_entity_sprite(
+    entity: Entity,
+    dimensions: (usize, usize),
+    buf: &mut Vec<DrawnColor>,
+    palette: &Palette,
+    prefer_existing: bool,
+) {
+    draw_entity_with_options(
+        entity,
+        dimensions,
+        buf,
+        palette,
+        true,
+        prefer_existing,
+        (8, 16),
+    );
 }
 
 pub fn draw_entity_with_options(
     entity: Entity,
     dimensions: (usize, usize),
-    buf: &mut Vec<DrawnColor>,
+    buffer: &mut Vec<DrawnColor>,
     palette: &Palette,
     transparency: bool,
     prefer_existing: bool,
+    origin: (usize, usize),
 ) {
-    for entity_y in 0..entity.height {
-        let y = entity_y + entity.y;
-        if y >= dimensions.1 {
-            continue;
+    let absolute_entity_y = entity.y.saturating_sub(origin.1); // 0
+    let absolute_entity_x = entity.x.saturating_sub(origin.0);
+
+    let starting_y = if entity.y < origin.1 {
+        origin.1 - entity.y
+    } else {
+        0
+    }; // 4
+
+    let starting_x = if entity.x < origin.0 {
+        origin.0 - entity.x
+    } else {
+        0
+    };
+
+    for entity_y in starting_y..entity.height {
+        let buffer_y = entity_y + absolute_entity_y - starting_y; // 0 + 0 -
+        if buffer_y >= dimensions.1 {
+            break;
         }
-        let base_idx = y * dimensions.0;
+
+        let base_buffer_idx = buffer_y * dimensions.0;
         let entity_base_idx = entity_y * entity.width;
-        for entity_x in 0..entity.width {
-            let x = entity.x + entity_x;
-            if x >= dimensions.0 {
-                continue;
+
+        for entity_x in starting_x..entity.width {
+            let buffer_x = absolute_entity_x + entity_x - starting_x;
+            if buffer_x >= dimensions.0 {
+                break;
             }
-            let buf_idx = base_idx + x;
+
+            let buffer_idx = base_buffer_idx + buffer_x;
             let entity_idx = entity_base_idx + entity_x;
-            let buffer_color = buf[buf_idx].color_value;
+            let buffer_color = buffer[buffer_idx].color_value;
             let entity_color = entity.data[entity_idx];
+
             if (!transparency || entity_color != 0) && (!prefer_existing || buffer_color == 0) {
-                buf[buf_idx] = DrawnColor {
+                buffer[buffer_idx] = DrawnColor {
                     color_value: entity_color,
                     color: palette.color(entity_color),
                 }
