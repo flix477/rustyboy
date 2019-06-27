514f137177dfdc0d351b0df0800a18cf05bd87fc
Félix Léveillé <flxleveille@gmail.com>
2019-06-26 22:29:35 -0400

    wip

--- packages/web/scripts/build-and-link.sh
+++ packages/web/scripts/build-and-link.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
 if ! command -v wasm-pack; then
    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh 
 fi
 wasm-pack build
+
+if [ ! -w pkg ]; then
+    chown -R $USER:$USER pkg
+fi
 
 cd pkg
 npm link
