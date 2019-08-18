97745d166e771e80beb8b84718f9deb642d7bbd9
Félix Léveillé <flxleveille@gmail.com>
2019-08-18 12:18:36 -0400

    fix tests

--- packages/core/src/cartridge/mbc/mbc3.rs
+++ packages/core/src/cartridge/mbc/mbc3.rs
@@ -154,7 +154,7 @@ mod tests {
     #[test]
     fn ram_bank_default() {
         let mbc = MBC3::new(&[]);
-        assert_eq!(mbc.relative_ram_address(0xA000), 0xA000);
+        assert_eq!(mbc.relative_ram_address(0xA000), 0);
     }
 
     #[test]
